ffe1c0eefdb4d2cfac5396a7c6789ea561323dd5
Dharaneesh017 <jagandharaneesh@gmail.com>
2025-07-13 23:30:43 +0530

    home component updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Gallery } from './pages/gallery/gallery';
+import { Members } from './pages/members/members';
+import { Contact } from './pages/contact/contact';
+import { Events } from './pages/events/events';
 
 export const routes: Routes = [
-    { path: '', component: Home}];
+    { path:'about',component:About},
+    { path:'gallery',component:Gallery},
+    { path:'members',component:Members},
+    {path:'contact',component:Contact},
+    { path:'events',component:Events},
+];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+isMobileMenuOpen = false;
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
